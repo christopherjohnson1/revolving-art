21fec2cee6207c31ab69171c592f5eba04968631
Chris Johnson <christopherjohnson1@gmail.com>
2020-09-21 12:01:59 -0400

    updated styling to navbar and landing text

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,7 +6,7 @@ import Navbar from 'react-bootstrap/Navbar'
 export const NavBar = (props) => {
     return (
         <>
-            <Navbar bg="light" variant="light" className="d-flex justify-content-end">
+            <Navbar bg="dark" variant="dark" className="d-flex justify-content-end">
                 <ul className="navbar__actual">
                     <li className="navbar__item active">
                         <Link className="navbar__link" to="/">Revolving Art</Link>

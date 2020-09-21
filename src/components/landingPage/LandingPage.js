e9a217ab8a68dcaafc3cc6400e81b52d29fd07f4
Chris Johnson <christopherjohnson1@gmail.com>
2020-09-21 11:21:23 -0400

    updated style and added text to landing image

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -5,9 +5,9 @@ export const LandingPage = () => {
     const landingImage = "https://res.cloudinary.com/cjflexmaster/image/upload/v1600650789/revolving-art/darya-tryfanava-5gSlzRZE3RQ-unsplash_yf8wsa.jpg"
 
     return (
-        <div>
-            <h1 className="text-center py-3">Welcome to Revolving Art</h1>
+        <div className="container">
             <div className="landingImage"><img src={ landingImage } alt="Small diner with art on the walls" style={{width: '1450px'}} /></div>
+            <div className="landing-page-text">Welcome to Revolving Art! Created to allow artists to have their work featured at local businesses! </div>
         </div>
     )
 }
